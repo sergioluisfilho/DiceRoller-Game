b4bd0f1a53b7d43b8bedd45453f867c77bdacebc
sergioluisfilho <sergioluisfilho@hotmail.com>
2020-07-19 00:14:22 -0300

    commit

---
+++ src/Die.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Die extends Component{
+    render(){
+        return (
+            <div>
+                <h1>DIE!</h1>
+                <i className="fas fa-dice-one"></i>
+            </div>
+        )
+    }
+}
+
+export default Die
